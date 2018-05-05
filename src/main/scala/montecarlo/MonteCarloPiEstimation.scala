beecf2a305e41f7d5262eef1e249bdff30b69686
philipshurpik <philip.shurpik@gmail.com>
2018-05-05 12:24:13 +0300

    monte carlo parallel 2 versions

--- src/main/scala/montecarlo/MonteCarloPiEstimation.scala
+++ src/main/scala/montecarlo/MonteCarloPiEstimation.scala
@@ -1,10 +1,9 @@
 package montecarlo
 
-import org.scalameter
-import parallel.parallel
-
-import scala.util.Random
 import org.scalameter._
+import parallel._
+import scala.util.Random
+
 
 object MonteCarloPiEstimation {
 
@@ -21,16 +20,25 @@ object MonteCarloPiEstimation {
         val x = rndX.nextDouble()
         val y = rndY.nextDouble()
 
-        simulate(hits + (if (x*x + y*y <= 1) 1 else 0), pointsGenerated + 1)
+        simulate(hits + (if (x * x + y * y <= 1) 1 else 0), pointsGenerated + 1)
       }
     }
 
     simulate(0, 0)
   }
 
-  def piParallelX(totalNumberOfPoints: Int): Double = {
-    val (pi1, pi2) = parallel(countPointsInsideCircle(totalNumberOfPoints/2), countPointsInsideCircle(totalNumberOfPoints/2))
+  def piParallel(totalNumberOfPoints: Int): Double = {
+    val (pi1, pi2) = parallel(countPointsInsideCircle(totalNumberOfPoints / 2), countPointsInsideCircle(totalNumberOfPoints / 2))
     4.0 * (pi1 + pi2) / totalNumberOfPoints
+  }
+
+  def piParallelX(totalNumberOfPoints: Int): Double = {
+    val count = Runtime.getRuntime.availableProcessors()
+
+    def parallelTask = () => countPointsInsideCircle(totalNumberOfPoints / count)
+
+    val results = List.fill(count)(parallelTask).par.map(x => x())
+    4.0 * results.sum / totalNumberOfPoints
   }
 
   def main(args: Array[String]): Unit = {
@@ -41,20 +49,27 @@ object MonteCarloPiEstimation {
       Key.exec.maxWarmupRuns -> 300,
       Key.exec.benchRuns -> 100,
       Key.verbose -> true
-    ).withWarmer(new scalameter.Warmer.Default)
+    ).withWarmer(new Warmer.Default)
 
     val seqtime = standardConfig.measure(
       pi(totalNumberOfPoints)
     )
 
     val partime = standardConfig.measure(
+      piParallel(totalNumberOfPoints)
+    )
+
+    val parXtime = standardConfig.measure(
       piParallelX(totalNumberOfPoints)
     )
 
-    println(s"pi sequential: ${pi(totalNumberOfPoints)}")
-    println(s"pi parallel:   ${piParallelX(totalNumberOfPoints)}")
-    println(s"sequential time $seqtime")
-    println(s"parallel time   $partime")
-    println(s"speedup: ${seqtime.value / partime.value}")
+    println(s"pi sequential:   ${pi(totalNumberOfPoints)}")
+    println(s"pi parallel:     ${piParallel(totalNumberOfPoints)}")
+    println(s"pi parallel X:   ${piParallelX(totalNumberOfPoints)}")
+    println(s"sequential time: $seqtime")
+    println(s"parallel time:   $partime")
+    println(s"parallel time:   $parXtime")
+    println(s"speedup:   ${seqtime.value / partime.value}")
+    println(s"speedup X: ${seqtime.value / parXtime.value}")
   }
 }
--- src/main/scala/parallel/parallel.scala
+++ src/main/scala/parallel/parallel.scala
@@ -21,7 +21,7 @@ package object parallel {
   }
 
   def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
-    val right = task { taskB }
+    val right = task(taskB)
     val left = taskA
 
     (left, right.join())
