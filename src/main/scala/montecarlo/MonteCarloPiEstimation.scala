c066ede301fc48c3d00fe1f06b7ca9c23d4adc50
philipshurpik <philip.shurpik@gmail.com>
2018-05-05 21:00:21 +0300

    simple refactoring

--- src/main/scala/montecarlo/MonteCarloPiEstimation.scala
+++ src/main/scala/montecarlo/MonteCarloPiEstimation.scala
@@ -7,9 +7,11 @@ import scala.util.Random
 
 object MonteCarloPiEstimation {
 
-  def pi(totalNumberOfPoints: Int): Double = 4.0 * countPointsInsideCircle(totalNumberOfPoints) / totalNumberOfPoints
+  private def getPi(squarePoints: Int, totalNumberOfPoints: Int): Double = 4.0 * squarePoints / totalNumberOfPoints
 
-  def countPointsInsideCircle(totalNumberOfPoints: Int): Int = {
+  def piSeq(totalNumberOfPoints: Int): Double = getPi(countPointsInsideCircle(totalNumberOfPoints), totalNumberOfPoints)
+
+  private def countPointsInsideCircle(totalNumberOfPoints: Int): Int = {
     val rndX = new Random()
     val rndY = new Random()
 
@@ -27,28 +29,28 @@ object MonteCarloPiEstimation {
     simulate(0, 0)
   }
 
-  def piParallel(totalNumberOfPoints: Int): Double = {
+  def piParallelSimple(totalNumberOfPoints: Int): Double = {
     val (pi1, pi2) = parallel(countPointsInsideCircle(totalNumberOfPoints / 2), countPointsInsideCircle(totalNumberOfPoints / 2))
-    4.0 * (pi1 + pi2) / totalNumberOfPoints
+    getPi(pi1 + pi2, totalNumberOfPoints)
   }
 
-  def piParallelX(totalNumberOfPoints: Int): Double = {
+  def piParallelParList(totalNumberOfPoints: Int): Double = {
     val count = Runtime.getRuntime.availableProcessors() - 1
 
     def parallelTask = () => countPointsInsideCircle(totalNumberOfPoints / count)
 
     val results = List.fill(count)(parallelTask).par.map(x => x())
-    4.0 * results.sum / totalNumberOfPoints
+    getPi(results.sum, totalNumberOfPoints)
   }
 
-  def piParallel8(totalNumberOfPoints: Int): Double = {
+  def piParallelTaskList(totalNumberOfPoints: Int): Double = {
     val count = Runtime.getRuntime.availableProcessors()
 
     def parallelTask = countPointsInsideCircle(totalNumberOfPoints / count)
 
     val taskList = List.fill(count - 1)(task(parallelTask))
     val mainTask = parallelTask
-    4.0 * (taskList.map(t => t.join()).sum + mainTask) / totalNumberOfPoints
+    getPi(taskList.map(t => t.join()).sum + mainTask, totalNumberOfPoints)
   }
 
   def main(args: Array[String]): Unit = {
@@ -57,36 +59,36 @@ object MonteCarloPiEstimation {
     val standardConfig = config(
       Key.exec.minWarmupRuns -> 50,
       Key.exec.maxWarmupRuns -> 200,
-      Key.exec.benchRuns -> 1000,
+      Key.exec.benchRuns -> 100,
       Key.verbose -> true
     ).withWarmer(new Warmer.Default)
 
     val seqtime = standardConfig.measure(
-      pi(totalNumberOfPoints)
+      piSeq(totalNumberOfPoints)
     )
 
-    val partime = standardConfig.measure(
-      piParallel(totalNumberOfPoints)
+    val parStime = standardConfig.measure(
+      piParallelSimple(totalNumberOfPoints)
     )
 
-    val parXtime = standardConfig.measure(
-      piParallelX(totalNumberOfPoints)
+    val parPLtime = standardConfig.measure(
+      piParallelParList(totalNumberOfPoints)
     )
 
-    val par8time = standardConfig.measure(
-      piParallel8(totalNumberOfPoints)
+    val parTLtime = standardConfig.measure(
+      piParallelTaskList(totalNumberOfPoints)
     )
 
-    println(s"pi sequential:   ${pi(totalNumberOfPoints)}")
-    println(s"pi parallel:     ${piParallel(totalNumberOfPoints)}")
-    println(s"pi parallel X:   ${piParallelX(totalNumberOfPoints)}")
-    println(s"pi parallel 8:   ${piParallel8(totalNumberOfPoints)}")
-    println(s"sequential time: $seqtime")
-    println(s"parallel time:   $partime")
-    println(s"parallel X time: $parXtime")
-    println(s"parallel 8 time: $par8time")
-    println(s"speedup:   ${seqtime.value / partime.value}")
-    println(s"speedup X: ${seqtime.value / parXtime.value}")
-    println(s"speedup 8: ${seqtime.value / par8time.value}")
+    println(s"pi sequential:    ${piSeq(totalNumberOfPoints)}")
+    println(s"pi parallel s:    ${piParallelSimple(totalNumberOfPoints)}")
+    println(s"pi parallel pl:   ${piParallelParList(totalNumberOfPoints)}")
+    println(s"pi parallel tl:   ${piParallelTaskList(totalNumberOfPoints)}")
+    println(s"sequential time:  $seqtime")
+    println(s"parallel s time:  $parStime")
+    println(s"parallel pl time: $parPLtime")
+    println(s"parallel tl time: $parTLtime")
+    println(s"speedup s:  ${seqtime.value / parStime.value}")
+    println(s"speedup pl: ${seqtime.value / parPLtime.value}")
+    println(s"speedup tl: ${seqtime.value / parTLtime.value}")
   }
 }
--- src/main/scala/parallel/parallel.scala
+++ src/main/scala/parallel/parallel.scala
@@ -26,4 +26,13 @@ package object parallel {
 
     (left, right.join())
   }
+
+  def parallel[A, B, C, D](taskA: => A, taskB: => B, taskC: => C, taskD: => D): (A, B, C, D) = {
+    val ta = task(taskA)
+    val tb = task(taskB)
+    val tc = task(taskC)
+    val td = taskD
+    (ta.join(), tb.join(), tc.join(), td)
+  }
+
 }
