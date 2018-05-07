b58c286fb3f535189007f0d203f5d59e1e7fc8d0
philipshurpik <philip.shurpik@gmail.com>
2018-05-07 14:44:50 +0300

    added speed tests

--- src/main/scala/words_counter/WordsCounter.scala
+++ src/main/scala/words_counter/WordsCounter.scala
@@ -1,7 +1,8 @@
 package words_counter
 
 import monoid_examples.Monoid
-import monoid_examples.foldMap.foldMapPar
+import monoid_examples.foldMap.{foldMapPar, foldMapSegment}
+import org.scalameter.{Key, Warmer, config}
 
 object WordsCounter {
   def main(args: Array[String]): Unit = {
@@ -11,7 +12,22 @@ object WordsCounter {
       "ing before the slower cars, in the Mille Miglia, however ",
       "the smaller displacement slower cars started first."
     )
-    println("Count:", wordsCounter(text51))
+    println(s"Count sequential:   ${wordsCounterSeq(text51)}")
+    println(s"Count parallel:     ${wordsCounter(text51)}")
+    println("*** Speed Measures ***")
+
+    val standardConfig = config(
+      Key.exec.minWarmupRuns -> 10,
+      Key.exec.maxWarmupRuns -> 30,
+      Key.exec.benchRuns -> 50,
+      Key.verbose -> true
+    ).withWarmer(new Warmer.Default)
+    val seqTime = standardConfig.measure(wordsCounterSeq(text51))
+    val parTime = standardConfig.measure(wordsCounter(text51))
+
+    println(s"sequential time:  $seqTime")
+    println(s"parallel time:    $parTime")
+    println(s"speedup           ${seqTime.value / parTime.value}")
   }
 
   def lineWordsCounter(line: String): (Boolean, Int, Boolean) = {
@@ -36,12 +52,17 @@ object WordsCounter {
     def zero: (Boolean, Int, Boolean) = (false, 0, false)
   }
 
+  private def combine(x: (Boolean, Int, Boolean)): Int = x._2 + (if (x._1) 1 else 0) + (if (x._3) 1 else 0)
+
+  def wordsCounterSeq(source: Vector[String]): Int = {
+    implicit val threshold: Int = 2
+    val monoid = makeMonoid()
+    combine(foldMapSegment[String, (Boolean, Int, Boolean)](source, from = 0, source.length, monoid)(lineWordsCounter))
+  }
+
   def wordsCounter(source: Vector[String]): Int = {
     implicit val threshold: Int = 2
     val monoid = makeMonoid()
-
-    val (onLeft, count, onRight) =
-      foldMapPar[String, (Boolean, Int, Boolean)](source, from = 0, source.length, monoid)(lineWordsCounter)
-    count + (if (onLeft) 1 else 0) + (if (onRight) 1 else 0)
+    combine(foldMapPar[String, (Boolean, Int, Boolean)](source, from = 0, source.length, monoid)(lineWordsCounter))
   }
 }
