84db260478fac457ec65c9fdc96a79582e57f83c
philipshurpik <philip.shurpik@gmail.com>
2018-05-08 17:35:03 +0300

    just call by value for easy debugging

--- src/main/scala/streams/Stream.scala
+++ src/main/scala/streams/Stream.scala
@@ -3,14 +3,14 @@ package streams
 sealed trait Stream[+A] {
   def headOption: Option[A] = this match {
     case Empty => None
-    case Cons(h, t) => Some(h())
+    case Cons(h, t) => Some(h)
   }
 
   def take(n: Int): Stream[A] = {
     println("n", this)
     this match {
-      case Cons(h, t) if n > 1 => Stream.cons(h(), t().take(n - 1))
-      case Cons(h, _) if n == 1 => Stream.cons(h(), Stream.empty)
+      case Cons(h, t) if n > 1 => Stream.cons(h, t.take(n - 1))
+      case Cons(h, _) if n == 1 => Stream.cons(h, Stream.empty)
       case _ => Stream.empty
     }
   }
@@ -18,7 +18,7 @@ sealed trait Stream[+A] {
   def toList: List[A] = {
     @annotation.tailrec
     def move(s: Stream[A], acc: List[A]): List[A] = s match {
-      case Cons(h, t) => move(t(), h() :: acc)
+      case Cons(h, t) => move(t, h :: acc)
       case _ => acc
     }
 
@@ -28,13 +28,13 @@ sealed trait Stream[+A] {
 
 case object Empty extends Stream[Nothing]
 
-case class Cons[+A](head: () => A, tail: () => Stream[A]) extends Stream[A]
+case class Cons[+A](head: A, tail: Stream[A]) extends Stream[A]
 
 object Stream {
   def cons[A](h: => A, t: => Stream[A]): Stream[A] = {
     lazy val head = h
     lazy val tail = t
-    Cons(() => head, () => tail)
+    Cons(head, tail)
   }
 
   def empty[A]: Stream[A] = Empty
