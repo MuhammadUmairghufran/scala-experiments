55ce0b7eaaf7818f92cd946c2d99c0fb2911c85d
philipshurpik <philip.shurpik@gmail.com>
2018-05-08 17:33:01 +0300

    streams initial

---
+++ src/main/scala/streams/StreamExamples.scala
@@ -0,0 +1,10 @@
+package streams
+
+object StreamExamples {
+  def main(args: Array[String]): Unit = {
+    val stream = Stream(1, 2, 3, 4, 5, 6, 7)
+    //println(stream.toList)
+    val top5 = stream.take(5)
+    println(top5.toList)
+  }
+}
